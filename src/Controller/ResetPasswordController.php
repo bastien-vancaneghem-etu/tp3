171ec1fd2b125ad3fff2ed66233d57f5490795c4
Bastien Vancaneghem <bastien.vancaneghem@etu.unicaen.fr>
2023-11-06 17:55:36 +0100

    redirect after reset password

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -120,7 +120,7 @@ class ResetPasswordController extends AbstractController
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
 
-            return $this->redirectToRoute('app_login');
+            return $this->redirectToRoute('app_tasks_index');
         }
 
         return $this->render('reset_password/reset.html.twig', [
