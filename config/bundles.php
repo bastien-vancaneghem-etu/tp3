9db7b8bf8552e411dced92e4a364c7faa8468a5d
Bastien Vancaneghem <bastien.vancaneghem@etu.unicaen.fr>
2023-11-06 16:51:15 +0100

    NEW : Register et passwords reset

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
